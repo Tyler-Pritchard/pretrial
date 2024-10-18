5b82cc3c5ffbc7c2c93ddc62de9d2287ee067af3
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-10-18 16:00:12 -0700

    camera following, configuring x-axis movement

--- Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -156,9 +156,6 @@ namespace StarterAssets
 			// set target speed based on move speed, sprint speed and if sprint is pressed
 			float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
 
-			// a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
-
-			// note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
 			// if there is no input, set the target speed to 0
 			if (_input.move == Vector2.zero) targetSpeed = 0.0f;
 
@@ -171,11 +168,7 @@ namespace StarterAssets
 			// accelerate or decelerate to target speed
 			if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
 			{
-				// creates curved result rather than a linear one giving a more organic speed change
-				// note T in Lerp is clamped, so we don't need to clamp our speed
 				_speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
-
-				// round speed to 3 decimal places
 				_speed = Mathf.Round(_speed * 1000f) / 1000f;
 			}
 			else
@@ -183,20 +176,24 @@ namespace StarterAssets
 				_speed = targetSpeed;
 			}
 
-			// normalise input direction
-			Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+			// Retrieve input direction for forward/backward movement
+			Vector3 inputDirection = transform.forward * _input.move.y; // Only Z-axis (forward/backward)
 
-			// note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-			// if there is a move input rotate player when the player is moving
-			if (_input.move != Vector2.zero)
+			// Rotate player based on horizontal input (left/right arrow keys)
+			if (_input.move.x != 0)
 			{
-				// move
-				inputDirection = transform.right * _input.move.x + transform.forward * _input.move.y;
+				// Rotate around Y-axis (turn the player left or right)
+				transform.Rotate(0, _input.move.x * RotationSpeed * Time.deltaTime, 0);
 			}
 
-			// move the player
-			_controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+			// Move the player forward/backward (ignoring X-axis for strafing)
+			if (_input.move != Vector2.zero)
+			{
+				// move forward/backward (using only Y input for Z-axis movement)
+				_controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+			}
 		}
+
 
 		private void JumpAndGravity()
 		{
